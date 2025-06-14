6f0c6f7133c167f2c8b41f17d30751f1ae24cef2
Anucha0096 <notpkvc6844@gmail.com>
2025-06-14 13:56:26 +0700

    Step 5: container, column, row, main axis, cross axis

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -4,18 +4,67 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  // Step 5: container, column, row, main axis, cross axis
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.blueAccent,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        padding: EdgeInsets.all(20),
-        child: Text(
-          "Hello Flutter",
-          style: TextStyle(fontSize: 30, letterSpacing: 3),
+    // return Column(
+      // mainAxisAlignment: MainAxisAlignment.center,
+      // mainAxisAlignment: MainAxisAlignment.start,
+      // mainAxisAlignment: MainAxisAlignment.end,
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      // crossAxisAlignment: CrossAxisAlignment.stretch,
+
+      
+      return Row(
+      //mainAxisAlignment: MainAxisAlignment.start,
+      //mainAxisAlignment: MainAxisAlignment.center,
+      //mainAxisAlignment: MainAxisAlignment.end,
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+
+      children: [
+        Container(
+          color: Colors.orangeAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              letterSpacing: 3,
+              color: Colors.white,
+            ),
+          ),
         ),
-      ),
+        
+        Container(
+          color: Colors.orangeAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: Text(
+            "Hellog",
+            style: TextStyle(
+              fontSize: 20,
+              letterSpacing: 3,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.orangeAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              letterSpacing: 3,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
